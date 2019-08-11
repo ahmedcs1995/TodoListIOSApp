bda443040416b8ec70fcf2fdce9bc665958dd4e6
yam tech <ahmedcs1995@gmail.com>
2019-08-11 12:50:46 -0700

    first commit

---
+++ TodoList/TaskAddViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TaskAddViewController.swift
+//  TodoList
+//
+//  Created by yam tech on 8/11/19.
+//  Copyright © 2019 yam tech. All rights reserved.
+//
+
+import UIKit
+
+class TaskAddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TodoList/TaskCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskCell.swift
+//  TodoList
+//
+//  Created by yam tech on 8/11/19.
+//  Copyright © 2019 yam tech. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
